a9847e800fe40584a741bcf96127bd540fb5b75e
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-11-07 22:14:50 +0400

    add empty files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+// import Head from "next/head";
+// import { ApolloProvider } from "@apollo/client";
+// import { useApollo } from "src/apollo";
+// import { AuthProvider } from "src/auth/useAuth";
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from 'next';
+// import { useState } from "react";
+// import { useQuery, gql } from "@apollo/client";
+// import { useDebounce } from "use-debounce";
+// import Layout from "src/components/layout";
+// import Map from "src/components/map";
+// import HouseList from "src/components/houseList";
+// import { useLastData } from "src/utils/useLastData";
+// import { useLocalState } from "src/utils/useLocalState";
+// import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
-const Home: NextPage = () => {
-  return <div className="">Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return <div>Home</div>;
+}
