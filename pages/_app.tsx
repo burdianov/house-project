b215d800bcfb9dfad9207cab68d934fa47cc29f6
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-11-10 11:26:01 +0400

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-// import Head from "next/head";
+import Head from 'next/head';
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
@@ -7,7 +7,15 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Home Sweet Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,49 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+/* eslint-disable @next/next/no-img-element */
+import { FunctionComponent, ReactNode } from 'react';
+import Link from 'next/link';
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  children: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ children }: IProps) => {
+  const authenticated = false;
+
+  const logout = () => {
+    return null;
+  };
+
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800 h-[64px]">
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="home house"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main className="screen-height-full-less-nav">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
