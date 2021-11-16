242d8ec2ff3431b7edffa614a55682fb1815b560
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-11-16 08:55:29 +0400

    complete houseForm

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,21 +1,8 @@
-// import { useState, useEffect, ChangeEvent } from "react";
-// import { useForm } from "react-hook-form";
-// import { useMutation, gql } from "@apollo/client";
-// import { useRouter } from "next/router";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
-// import {
-//   CreateHouseMutation,
-//   CreateHouseMutationVariables,
-// } from "src/generated/CreateHouseMutation";
-// import {
-//   UpdateHouseMutation,
-//   UpdateHouseMutationVariables,
-// } from "src/generated/UpdateHouseMutation";
-// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
-import { useEffect, useState } from 'react';
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState, ChangeEvent } from 'react';
 import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+
 import { SearchBox } from './searchBox';
 
 interface IFormData {
@@ -30,6 +17,7 @@ interface IProps {}
 
 const HouseForm = ({}: IProps) => {
   const [submitting, setSubmitting] = useState<boolean>(false);
+  const [previewImage, setPreviewImage] = useState<string>();
   const {
     register,
     handleSubmit,
@@ -48,7 +36,9 @@ const HouseForm = ({}: IProps) => {
     register('longitude', { required: true, min: -180, max: 180 });
   }, [register]);
 
-  const handleCreate = async (data: IFormData) => {};
+  const handleCreate = async (data: IFormData) => {
+    console.log({ data });
+  };
 
   const onSubmit = (data: IFormData) => {
     setSubmitting(true);
@@ -74,9 +64,89 @@ const HouseForm = ({}: IProps) => {
           Search for Your Address
         </label>
         <SearchBox onSelectAddress={onSelectAddress} defaultValue="" />
-        {errors.address && <p>{errors.address.message}</p>}
-        <h2>{address}</h2>
+        {errors.address && (
+          <p className="error-message">{errors.address.message}</p>
+        )}
       </div>
+
+      {address && (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              Click to add image (16:9)
+            </label>
+            <input
+              id="image"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              {...register('image', {
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) {
+                    return true;
+                  }
+                  return 'Please upload one file';
+                },
+                onChange: (event: ChangeEvent<HTMLInputElement>) => {
+                  if (event?.target?.files?.[0]) {
+                    const file = event.target.files[0];
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                      setPreviewImage(reader.result as string);
+                    };
+                    reader.readAsDataURL(file);
+                  }
+                }
+              })}
+            />
+            {previewImage && (
+              <img
+                src={previewImage}
+                alt="house"
+                className="mt-4 object-cover w-[576px]"
+                style={{ width: '576px', height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+            {errors.image && (
+              <p className="error-message">{errors.image.message}</p>
+            )}
+          </div>
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              id="bedrooms"
+              type="number"
+              className="p-2"
+              {...register('bedrooms', {
+                required: 'Please enter the number of bedrooms',
+                max: { value: 10, message: 'Wooahh, too big of a house' },
+                min: { value: 1, message: 'Must have at least 1 bedroom' }
+              })}
+            />
+            {errors.bedrooms && (
+              <p className="error-message">{errors.bedrooms.message}</p>
+            )}
+          </div>
+
+          <div className="mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded mr-2"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>
+            <Link href="/">
+              <a>Cancel</a>
+            </Link>
+          </div>
+        </>
+      )}
     </form>
   );
 };
